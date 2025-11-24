4c8800d000dca292f0fcd681afcbb7881fb240f9
Mary Felvie <mary.feliciano@gmail.com>
2025-11-24 16:50:54 -0300

    add gameBoard functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import { useState } from "react";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns]= useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
+  
+  function handleSelectSquare(rowIndex, colIndex) { 
+    setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "0" : "X");
+    setGameTurns(prevTurns => {
+      let currentPlayer = "X";
+      if(prevTurns.length>0 && prevTurns[0].player === "X"){
+        currentPlayer = "0"
+      }
+      const updateTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updateTurns;
+    })
+  }
+  
   return (
     <>
       <main>
         <div id="game-container">
-          <ol id="players">
-            <Player initialName="Player 1" symbol="X" />
-            <Player initialName="Player 2" symbol="0" />
+          <ol id="players" className="highlight-player">
+            <Player initialName="Player 1" symbol="X" isActive={ activePlayer === 'X'} />
+            <Player initialName="Player 2" symbol="0" isActive={ activePlayer === '0'} />
           </ol>
-          <GameBoard />
+          <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={ activePlayer } />
         </div>
-        log
+        <Log />
       </main>
     </>
   );
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,46 @@
+import { useState } from "react";
+
+//lifting state up example
 const initilGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initilGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      //boa pratica sempre que precisar atualizar um
+      // objeto/array dentro do estado , deve-se atualizar
+      // a copia dele e nao ele em si ja que e os valores dele sao valores por referencia
+      // e nao por valor, deve-se atualizar o objeto de forma imutavel
+      //ler mais sobre update object ou array state values in react doc
+
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }
+
   return (
     <>
       <ol id="game-board">
-        {initilGameBoard.map((row, rowIndex) => (
+        {gameBoard.map((row, rowIndex) => (
           <li key={rowIndex}>
             <ol>
               {row.map((playerSymbol, colIndex) => (
-                <li key={colIndex} className="cell">
-                  <button>{playerSymbol}</button>
+                <li key={colIndex}>
+                  <button
+                    onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                  >
+                    {playerSymbol}
+                  </button>
                 </li>
               ))}
             </ol>
