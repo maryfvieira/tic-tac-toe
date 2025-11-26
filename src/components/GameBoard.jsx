74fa1a04b160b1ff7f01c68c31a3575aa37d6e34
Mary Felvie <mary.feliciano@gmail.com>
2025-11-26 18:10:10 -0300

    removing duplicated code

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,52 +1,35 @@
-import { useState } from "react";
 
 //lifting state up example
-const initilGameBoard = [
+const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initilGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      //boa pratica sempre que precisar atualizar um
-      // objeto/array dentro do estado , deve-se atualizar
-      // a copia dele e nao ele em si ja que e os valores dele sao valores por referencia
-      // e nao por valor, deve-se atualizar o objeto de forma imutavel
-      //ler mais sobre update object ou array state values in react doc
-
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-
-    onSelectSquare();
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
   }
-
-  return (
-    <>
-      <ol id="game-board">
-        {gameBoard.map((row, rowIndex) => (
-          <li key={rowIndex}>
-            <ol>
-              {row.map((playerSymbol, colIndex) => (
-                <li key={colIndex}>
-                  <button
-                    onClick={() => handleSelectSquare(rowIndex, colIndex)}
-                  >
-                    {playerSymbol}
-                  </button>
-                </li>
-              ))}
-            </ol>
-          </li>
-        ))}
-      </ol>
-    </>
-  );
+  
+    return (
+      <>
+        <ol id="game-board">
+          {gameBoard.map((row, rowIndex) => (
+            <li key={rowIndex}>
+              <ol>
+                {row.map((playerSymbol, colIndex) => (
+                  <li key={colIndex}>
+                    <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                  </li>
+                ))}
+              </ol>
+            </li>
+          ))}
+        </ol>
+      </>
+    );
+  
 }
